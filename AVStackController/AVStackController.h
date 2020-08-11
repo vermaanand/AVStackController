c2e86d431d976f54c9954034eb5871e6f8e1fec3
Anand Verma <vermaanand607@gmail.com>
2020-08-11 11:42:24 +0530

    Project Setup

---
+++ AVStackController/AVStackController.h
@@ -0,0 +1,19 @@
+//
+//  AVStackController.h
+//  AVStackController
+//
+//  Created by Anand Verma on 11/08/20.
+//  Copyright © 2020 AVSwiftHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AVStackController.
+FOUNDATION_EXPORT double AVStackControllerVersionNumber;
+
+//! Project version string for AVStackController.
+FOUNDATION_EXPORT const unsigned char AVStackControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVStackController/PublicHeader.h>
+
+
---
+++ AVStackController/Sources/BottomPopupDismissAnimator.swift
@@ -0,0 +1,33 @@
+//
+//  DraggableDismissAnimator.swift
+//  CREDAssignment
+//
+//  Created by Anand Verma on 10/08/20.
+//  Copyright © 2020 CRED. All rights reserved.
+//
+
+
+import UIKit
+
+final class BottomPopupDismissAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    private unowned var attributesOwner: BottomPresentableViewController
+    
+    init(attributesOwner: BottomPresentableViewController) {
+        self.attributesOwner = attributesOwner
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return attributesOwner.popupDismissDuration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let fromVC = transitionContext.viewController(forKey: .from)!
+        let dismissFrame = CGRect(origin: CGPoint(x: 0, y: UIScreen.main.bounds.size.height), size: fromVC.view.frame.size)
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            fromVC.view.frame = dismissFrame
+        }) { (_) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+    }
+}
